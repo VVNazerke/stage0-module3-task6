025114d491a0519a707d53f3be8a08317764d356
admin <nbisametova@mail.ru>
2022-08-28 17:02:11 +0600

    the first try

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -1,6 +1,10 @@
 package lang.print.gaps.task6;
 
+import static java.lang.Math.round;
+
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+        int myInt = round(numberToBeRounded);
+        System.out.println(myInt);
     }
 }
